8249db6618408d743d2e5895534bcb30c5dff27b
madamou <madamou@inception.myguest.virtualbox.org>
2025-01-06 20:48:09 +0100

    improve inception

--- srcs/requirements/wordpress/wordpress.sh
+++ srcs/requirements/wordpress/wordpress.sh
@@ -1,47 +1,23 @@
 #!/bin/sh
 
-#!/bin/bash
-
 sleep 5
-
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
-
-mkdir /var/www/html/wordpress/
-
-cd /var/www/html/wordpress/
-chmod -R 755 /var/www/html/wordpress/
-adduser -u 82 -D -G www-data www-data
-chown -R www-data:www-data /var/www/html/wordpress
 
 
 echo "[========WP INSTALLATION STARTED========]"
-
-#!/bin/sh
-
-# Chemin vers WordPress
-WORDPRESS_PATH="/var/www/html/wordpress"
-
-# Vérifie si le fichier wp-config-sample.php existe
-if [ ! -f "$WORDPRESS_PATH/wp-config.php" ]; then
+if [ ! -f "/var/www/html/wordpress/wp-config.php" ]; then
     echo "wp-config.php non trouvé. Initialisation de WordPress..."
 
-    # Supprime tous les fichiers existants dans le répertoire WordPress
-    find "$WORDPRESS_PATH/" -mindepth 1 -delete
+    find "/var/www/html/wordpress/" -mindepth 1 -delete
 
-    # Télécharge le core WordPress
     wp core download --allow-root
 
-    # Configure WordPress avec les informations de la base de données
     wp core config \
-        --dbhost="mariadb:3306" \
+        --dbhost="$MARIADB_HOST" \
         --dbname="$MARIADB_NAME" \
         --dbuser="$MARIADB_USER" \
         --dbpass="$MARIADB_USER_PASSWORD" \
         --allow-root
 
-    # Installe WordPress avec les informations fournies
     wp core install \
         --url="$DOMAIN_NAME" \
         --title="$TITLE" \
@@ -50,7 +26,6 @@ if [ ! -f "$WORDPRESS_PATH/wp-config.php" ]; then
         --admin_email="$WP_ADMIN_EMAIL" \
         --allow-root
 
-    # Crée un utilisateur WordPress supplémentaire
     wp user create \
         "$WP_USER" \
         "$WP_USER_EMAIL" \
