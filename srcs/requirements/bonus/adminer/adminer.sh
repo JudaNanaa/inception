0262f2ad87b43872452dd627aea52e62c94a7d45
madamou <madamou@inception.myguest.virtualbox.org>
2025-01-13 21:04:39 +0100

    finish adminer

--- srcs/requirements/bonus/adminer/adminer.sh
+++ srcs/requirements/bonus/adminer/adminer.sh
@@ -1,12 +1,13 @@
-#!bin/bash
+#!/bin/sh
 
-
-wget "http://www.adminer.org/latest.php" -O /var/www/html/adminer.php 
-chown -R www-data:www-data /var/www/html/adminer.php 
+mkdir -p /var/www/html/
+wget "http://www.adminer.org/latest.php" -O /var/www/html/adminer.php
+adduser -u 82 -D -G www-data www-data
+chown -R www-data:www-data /var/www/html/adminer.php
 chmod 755 /var/www/html/adminer.php
-
 
 cd /var/www/html
 
 rm -rf index.html
+
 php -S 0.0.0.0:80
