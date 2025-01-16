eabc33f032bda0a43226b8d7eaf3cbde9dd2134d
madamou <madamou@inception.myguest.virtualbox.org>
2025-01-16 17:52:26 +0100

    i gonna push

--- srcs/requirements/bonus/backup/conf/backup.sh
+++ srcs/requirements/bonus/backup/conf/backup.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
-# Variables
 BACKUP_DIR="/backup/$(date +'%Y-%m-%d_%H-%M-%S')"
-DB_HOST="mariadb" # Nom du container MariaDB
 
-# Crée le dossier de backup
 mkdir -p "$BACKUP_DIR"
 
-# Sauvegarde de la base de données
 mysqldump -h $DB_HOST -u $MARIADB_USER -p$MARIADB_USER_PASSWORD $MARIADB_NAME > "$BACKUP_DIR/db_backup.sql"
 
-# Sauvegarde des fichiers WordPress (volume partagé)
 tar -czf "$BACKUP_DIR/wp_backup.tar.gz" /var/www/html
 
-# (Optionnel) Supprime les backups de plus de 7 jours
 find /backup/* -mtime +7 -exec rm -rf {} \;
 
 echo "Backup terminé à $(date)"
